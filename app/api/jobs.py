075fe59ff65e668a3908f5547e4e84e4ff0bc6e6
Abhi10111 <abhishekaggarwal896@gmail.com>
2025-12-22 15:53:35 +0000

    feat: Adding job lifecylce and timestamps

--- app/api/jobs.py
+++ app/api/jobs.py
@@ -1,18 +1,51 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 from typing import List
 from uuid import uuid4
-
-from app.schemas.job import Job,JobCreate
+from datetime import datetime
+from app.schemas.job import Job,JobCreate,JobState
 
 router=APIRouter(prefix='/jobs',tags=["Jobs"])
 
-JOBS_DB=[]
+JOBS_DB:List[Job]=[]
 
 @router.post("/",response_model=Job)
 def create_job(job:JobCreate):
-    new_job=Job(id=uuid4(),**job.dict())
+    new_job=Job(id=uuid4(),state=JobState.PENDING,created_at=datetime.utcnow(),**job.dict())
     JOBS_DB.append(new_job)
     return new_job
+
+@router.post("/{job_id}/start",response_model=Job)
+def start_job(job_id:str):
+    for job in JOBS_DB:
+        if str(job.id) == job_id:
+            if job.state != JobState.PENDING:
+                raise HTTPException(status_code=400,detail="Only PENDING jobs can be started")
+            job.state=JobState.RUNNING
+            job.started_at=datetime.utcnow()
+            return job
+    raise HTTPException(status_code=404,detail="Job not found")
+
+@router.post("/{job_id}/complete",response_model=Job)
+def complete_job(job_id:str):
+    for job in JOBS_DB:
+        if str(job.id) == job_id:
+            if job.state != JobState.RUNNING:
+                raise HTTPException(status_code=400,detail="Only RUNNING jobs can be completed")
+            job.state=JobState.COMPLETED
+            job.finished_at=datetime.utcnow()
+            return job
+    raise HTTPException(status_code=404,detail="Job not found")
+
+@router.post("/{job_id}/fail",response_model=Job)
+def complete_job(job_id:str):
+    for job in JOBS_DB:
+        if str(job.id)== job_id:
+            if job.state != JobState.RUNNING:
+                raise HTTPException(status_code=400,detail="Only RUNNING jobs can fail")
+            job.state=JobState.FAILED
+            job.finished_at=datetime.utcnow()
+            return job
+    raise HTTPException(status_code=404,detail="Job not found")
 
 @router.get("/",response_model=List[Job])
 def list_jobs():
--- app/schemas/job.py
+++ app/schemas/job.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 from uuid import UUID, uuid4
+from enum import Enum
+
 
 class JobCreate(BaseModel):
     title:str
@@ -8,6 +11,17 @@ class JobCreate(BaseModel):
     location:str
     description:Optional[str]=None
 
+class JobState(str, Enum):
+    PENDING = "PENDING"
+    RUNNING = "RUNNING"
+    COMPLETED = "COMPLETED"
+    FAILED = "FAILED"
+    
 class Job(JobCreate):
     id:UUID
+    state:JobState
+    created_at:datetime
+    started_at:Optional[datetime]=None
+    finished_at:Optional[datetime]=None
+
 
